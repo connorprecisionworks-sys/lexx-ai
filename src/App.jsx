8832de8acce2d909e8a500cafc3b20cea304ae75
Connor <youremail@gmail.com>
2026-03-30 13:51:12 -0400

    Move Lenis to App level, fix scroll stacking

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,6 @@
+import { useEffect } from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Lenis from '@studio-freight/lenis';
 import Navbar from './components/Navbar';
 import Home from './pages/Home';
 import HowItWorks from './pages/HowItWorks';
@@ -8,6 +10,22 @@ import Contact from './pages/Contact';
 import './index.css';
 
 export default function App() {
+  useEffect(() => {
+    const lenis = new Lenis({
+      duration: 1.2,
+      easing: (t) => Math.min(1, 1.001 - Math.pow(2, -10 * t)),
+      smooth: true,
+    });
+
+    function raf(time) {
+      lenis.raf(time);
+      requestAnimationFrame(raf);
+    }
+    requestAnimationFrame(raf);
+
+    return () => lenis.destroy();
+  }, []);
+
   return (
     <BrowserRouter>
       <Navbar />
